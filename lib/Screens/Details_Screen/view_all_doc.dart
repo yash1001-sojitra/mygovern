a85526de3c299f7032c6d60fc8f5bdbaa86f6adf
yash sojitra <97076802+yash1001-sojitra@users.noreply.github.com>
2022-09-19 19:07:02 +0530

    doc2 fix

--- lib/Screens/Details_Screen/view_all_doc.dart
+++ lib/Screens/Details_Screen/view_all_doc.dart
@@ -83,7 +83,7 @@ class _ViewAllDocState extends State<ViewAllDoc> {
           // padding: EdgeInsets.all(10),
           shrinkWrap: true,
           itemBuilder: (BuildContext context, int index) {
-            return DocCard3(
+            return DocCard2(
                 documentname: documentsname[index],
                 documentimage: "assets/images/Mygov.png");
           },
@@ -93,60 +93,60 @@ class _ViewAllDocState extends State<ViewAllDoc> {
   }
 }
 
-class DocCard3 extends StatelessWidget {
-  String documentname;
-  String documentimage;
-  DocCard3(
-      {required this.documentname, required this.documentimage, super.key});
+// class DocCard3 extends StatelessWidget {
+//   String documentname;
+//   String documentimage;
+//   DocCard3(
+//       {required this.documentname, required this.documentimage, super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 130,
-      height: 200,
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey.withOpacity(0.4)),
-                  borderRadius: BorderRadius.circular(50)),
-              child: CircleAvatar(
-                backgroundColor: Colors.white,
-                radius: 40,
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 8.0, left: 10),
-                  child: Image.asset(
-                    documentimage,
-                    height: 60,
-                    width: 60,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Flexible(
-                    child: Text(documentname,
-                        softWrap: true,
-                        maxLines: 2,
-                        overflow: TextOverflow.clip,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.w900,
-                            color: Color.fromARGB(255, 93, 23, 105)))),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       width: 130,
+//       height: 200,
+//       decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+//       child: Padding(
+//         padding: const EdgeInsets.all(8.0),
+//         child: Column(
+//           children: [
+//             Container(
+//               decoration: BoxDecoration(
+//                   border: Border.all(color: Colors.grey.withOpacity(0.4)),
+//                   borderRadius: BorderRadius.circular(50)),
+//               child: CircleAvatar(
+//                 backgroundColor: Colors.white,
+//                 radius: 40,
+//                 child: Padding(
+//                   padding: const EdgeInsets.only(top: 8.0, left: 10),
+//                   child: Image.asset(
+//                     documentimage,
+//                     height: 60,
+//                     width: 60,
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             SizedBox(
+//               height: 10,
+//             ),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 Flexible(
+//                     child: Text(documentname,
+//                         softWrap: true,
+//                         maxLines: 2,
+//                         overflow: TextOverflow.clip,
+//                         textAlign: TextAlign.center,
+//                         style: TextStyle(
+//                             fontSize: 15,
+//                             fontWeight: FontWeight.w900,
+//                             color: Color.fromARGB(255, 93, 23, 105)))),
+//               ],
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
